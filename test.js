f4b666352e0e5b231fd2cecb3f998db34b1a9472
Kevin Martensson <kevinmartensson@gmail.com>
2017-06-14 15:43:47 +0200

    Meta tweaks

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 import test from 'ava';
-import m from './';
+import m from '.';
 
 test(async t => {
-	const pkgs = await m('kevva');
+	const [pkg, ...pkgs] = await m('kevva');
 	t.true(pkgs.length > 100);
-	t.truthy(pkgs[0].name);
-	t.truthy(pkgs[0].description);
+	t.truthy(pkg.name);
+	t.truthy(pkg.description);
 });
